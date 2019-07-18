5f8b562e784511fbd40373f53f0fcfa939dd87f2
biobod <info96532@gmail.com>
2019-07-18 18:51:05 +0300

    Updated Account profile page

--- frontend/src/components/AccountProfile.js
+++ frontend/src/components/AccountProfile.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import { getUser } from '../APIUtilites/clientQuery';
 
 class AccountProfile extends Component {
   render() {
     return (
       <div>
-        Account Profile page
+        <Query query={getUser}>
+          {({ data: { user }, loading, error }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error || !user) return <div> no user found</div>;
+            return (
+              <div>
+                <p>Account Profile page</p>
+                <div>
+                  <span>Name: </span>
+                  <span>{user.name}</span>
+                </div>
+                <div>
+                  <span>Email: </span>
+                  <span>{user.email}</span>
+                </div>
+
+                <div>
+                  <span>id: </span>
+                  <span>{user._id}</span>
+                </div>
+
+              </div>
+            );
+          }}
+        </Query>
       </div>
     );
   }
