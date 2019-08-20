6645849309caa2d76a6acb4be746856d89f388f2
biobod <info96532@gmail.com>
2019-08-20 16:16:25 +0300

    Updated webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -11,9 +11,10 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   devServer: {
-    contentBase: 'dist',
+    historyApiFallback: true,
   },
   resolve: {
     alias: {
